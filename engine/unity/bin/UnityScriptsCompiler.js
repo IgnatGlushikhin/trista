8709c07c118a87fa8589017c092dc7d231729583
dudkin <dudkin@game-insight.com>
2021-04-22 11:57:32 +0300

    Converted 07097fed commit from main repo

--- engine/unity/bin/UnityScriptsCompiler.js
+++ engine/unity/bin/UnityScriptsCompiler.js
@@ -1,5 +1,5 @@
 /**
- * @version 1.0.7781.42521
+ * @version 1.0.7782.21171
  * @copyright anton
  * @compiler Bridge.NET 17.9.11-luna
  */
@@ -4360,7 +4360,7 @@ Bridge.assembly("UnityScriptsCompiler", function ($asm, globals) {
             /*GameManager.Awake start.*/
             Awake: function () {
                 var $t;
-                this._WebContentProxy.inputPayload = "InputPayloadReplacementStub";
+                this._WebContentProxy.inputPayload = inputJsDataPayload;
                 this._WebContentProxy.DecodeInputPayload();
 
                 var trackIndex = this.GetInputProperties().trackIndex;
@@ -11287,7 +11287,7 @@ Bridge.assembly("UnityScriptsCompiler", function ($asm, globals) {
                 this.$initialize();
                 // debug way of init (will be overriden in not debug case)
                 this.inputProperties.trackIndex = 0;
-                this.inputProperties.vehicleIndex = 1;
+                this.inputProperties.vehicleIndex = 0;
                 this.inputProperties.driverIndex = 0;
                 this.inputProperties.gemCount = 100;
             }
@@ -11308,7 +11308,7 @@ Bridge.assembly("UnityScriptsCompiler", function ($asm, globals) {
             /*WebContentProxy.CallFinishWebContentFunction start.*/
             CallFinishWebContentFunction: function () {
                 this.EncodeOutputPayload();
-                UnityEngine.Debug.Log$1("Here is finish call with payload:" + (this.outputPayload || ""));
+                closeWebContent(this.outputPayload);
             },
             /*WebContentProxy.CallFinishWebContentFunction end.*/
 
